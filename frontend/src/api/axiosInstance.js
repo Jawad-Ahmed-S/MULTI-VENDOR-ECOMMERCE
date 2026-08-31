540c24af1bf70a9ffd283eddaaf4093dbc4654ca
Jawad Ahmed <soomrojawad1.2.2006@gmail.com>
2026-08-31 21:16:50 +0500

    route fixed

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import { store } from "../redux/store.js"
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api/v1/`,
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/`,
 })
  
 api.interceptors.request.use((config) => {
